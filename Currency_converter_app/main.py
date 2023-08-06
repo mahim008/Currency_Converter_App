ac73efb14b0e85096cc121f764fa05978e25b608
Mahim <mahim.cse.uu@outlook.com>
2023-08-06 20:30:28 +0600

    added labels for ui

--- Currency_converter_app/main.py
+++ Currency_converter_app/main.py
@@ -5,5 +5,8 @@ class CurrencyConverter:
         app_window = Tk()
         app_window.title("Currency Converter")
         app_window.configure(bg="yellow")
+        Label(app_window, font="Helvetica 12 bold", bg="yellow", text="Amount to Convert").grid(row = 1, column = 1, stricky = W)
+        Label(app_window, font="Helvetica 12 bold", bg="yellow", text="Conversion Rate").grid(row = 2, column = 1, stricky = W)
+        Label(app_window, font="Helvetica 12 bold", bg="yellow", text="Converted Amount").grid(row = 3, column = 1, stricky = W)
 
 
