6ccc702f7f48df9be0d5fd4d740274c79793e43a
Mahim <mahim.cse.uu@outlook.com>
2023-08-06 20:15:55 +0600

    built the app

---
+++ Currency_converter_app/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class CurrencyConverter:
+    def __int__(self):
+        app_window = Tk()
+        app_window.title("Currency Converter")
+        app_window.configure(bg="yellow")
+
+
